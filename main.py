0b5ece1dca0f722648327e2edd031d1e127dde90
けんたろう <71170923+Kentaro1043@users.noreply.github.com>
2020-11-13 20:34:02 +0900

    main

--- main.py
+++ main.py
@@ -2,13 +2,11 @@ import kivy
 kivy.require('2.0.0')
 
 from kivy.app import App
-from kivy.uix.label import Label
 
 
-class MyApp(App):
+class timerApp(App):
 
-    def build(self):
-        return Label(text='Hallo World')
+    pass
 
 if __name__ == '__main__':
-    MyApp().run()
+    timerApp().run()
