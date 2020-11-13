463143c91af0eb9255d4fa5eea9d889b6599ddf8
けんたろう <71170923+Kentaro1043@users.noreply.github.com>
2020-11-13 21:26:55 +0900

    main

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ kivy.require('2.0.0')
 from kivy.app import App
 
 
-class timerApp(App):
+class MyApp(App):
 
-    pass
+    def __init__(self, **kwargs):
+        super(MyApp, self).__init__(**kwargs)
+        self.title = 'timer.py'
 
 if __name__ == '__main__':
-    timerApp().run()
+    MyApp().run()
