038da1c8b1b06d7c2a154d194c7ff223a8b03948
けんたろう <71170923+Kentaro1043@users.noreply.github.com>
2020-12-13 19:39:38 +0900

    main

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ kivy.require('2.0.0')
 
 from kivy.app import App
 from kivy.uix.widget import Widget
-
 from kivy.properties import StringProperty
+from kivy.clock import Clock
 
 hour = 0
 minute = 0
@@ -19,15 +19,35 @@ class TextWidget(Widget):
         super(TextWidget, self).__init__(**kwargs)
         self.text = ""
 
-    def buttonClicked(self):
+    def intervalCountdown(self,dt):
+        self.text = str(int(self.text) - 1)
+        if int(self.text) == -1:
+            self.text = "finished"
+            self.ids["stopButton"].disabled = True
+            self.ids["restartButton"].disabled = True
+            self.ids["startButton"].disabled = False
+            return False
+
+    def startButton(self):
         hour = int(self.ids["hour_box"].text)
         minute = int(self.ids["minute_box"].text)
         second = int(self.ids["second_box"].text)
         allTime = hour * 3600 + minute * 60 + second
-        for i in range(allTime,-1,-1):
-            self.text = str(i)
-            time.sleep(1)
-        self.text = "finished"
+        self.text = str(allTime)
+        self.ids["stopButton"].disabled = False
+        self.ids["restartButton"].disabled = True
+        self.ids["startButton"].disabled = True
+        Clock.schedule_interval(self.intervalCountdown,1)
+
+    def stopButton(self):
+        Clock.unschedule(self.intervalCountdown)
+        self.ids["stopButton"].disabled = True
+        self.ids["restartButton"].disabled = False
+    
+    def restartButton(self):
+        Clock.schedule_interval(self.intervalCountdown,1)
+        self.ids["stopButton"].disabled = False
+        self.ids["restartButton"].disabled = True
 
 class MyApp(App):
 
@@ -36,6 +56,7 @@ class MyApp(App):
         self.title = 'timer.py'
     
     def build(self):
+        self.icon = 'icon.ico'
         return TextWidget()
 
 if __name__ == '__main__':
