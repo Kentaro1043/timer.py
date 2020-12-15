399e3fc241e91656aa89ddfcb5c2188c23a8f3d5
けんたろう <71170923+Kentaro1043@users.noreply.github.com>
2020-12-15 20:56:20 +0900

    main

--- main.py
+++ main.py
@@ -24,29 +24,46 @@ class TextWidget(Widget):
         if int(self.text) == -1:
             self.text = "finished"
             self.ids["stopButton"].disabled = True
+            self.ids["pauseButton"].disabled = True
             self.ids["restartButton"].disabled = True
             self.ids["startButton"].disabled = False
             return False
 
     def startButton(self):
-        hour = int(self.ids["hour_box"].text)
-        minute = int(self.ids["minute_box"].text)
-        second = int(self.ids["second_box"].text)
+        try:
+            hour = int(self.ids["hour_box"].text)
+            minute = int(self.ids["minute_box"].text)
+            second = int(self.ids["second_box"].text)
+        except ValueError:
+            self.text = "illegal value!"
+            return 0
         allTime = hour * 3600 + minute * 60 + second
+        if allTime < 0:
+            self.text = "illegal value!"
+            return 0
         self.text = str(allTime)
         self.ids["stopButton"].disabled = False
+        self.ids["pauseButton"].disabled = False
         self.ids["restartButton"].disabled = True
         self.ids["startButton"].disabled = True
         Clock.schedule_interval(self.intervalCountdown,1)
 
     def stopButton(self):
         Clock.unschedule(self.intervalCountdown)
+        self.text = ""
         self.ids["stopButton"].disabled = True
+        self.ids["pauseButton"].disabled = True
+        self.ids["restartButton"].disabled = True
+        self.ids["startButton"].disabled = False
+
+    def pauseButton(self):
+        Clock.unschedule(self.intervalCountdown)
+        self.ids["pauseButton"].disabled = True
         self.ids["restartButton"].disabled = False
     
     def restartButton(self):
         Clock.schedule_interval(self.intervalCountdown,1)
-        self.ids["stopButton"].disabled = False
+        self.ids["pauseButton"].disabled = False
         self.ids["restartButton"].disabled = True
 
 class MyApp(App):
