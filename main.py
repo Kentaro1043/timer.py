92fa5563d02c36c3a713f84f463651472ed858ec
けんたろう <71170923+Kentaro1043@users.noreply.github.com>
2020-12-20 21:44:04 +0900

    main

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import time
+import math
 
 import kivy
 kivy.require('2.0.0')
@@ -7,6 +8,7 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.properties import StringProperty
 from kivy.clock import Clock
+from kivy.core.audio import SoundLoader
 
 hour = 0
 minute = 0
@@ -19,29 +21,53 @@ class TextWidget(Widget):
         super(TextWidget, self).__init__(**kwargs)
         self.text = ""
 
+    def _on_stop(self):
+        self.sound.play()
+
+    def passFunction(self):
+        pass
+
     def intervalCountdown(self,dt):
-        self.text = str(int(self.text) - 1)
-        if int(self.text) == -1:
+        self.allTime = self.allTime - 1
+        self.hourText = "00" + str(math.floor(int(self.allTime) / 3600))
+        self.minuteText = "00" + str(math.floor(int(self.allTime) % 3600 / 60))
+        self.secondText = "00" + str(math.floor(int(self.allTime) %3600 % 60))
+        self.text = self.hourText[-2:] + ":" + self.minuteText[-2:] + ":" + self.secondText[-2:]
+        if self.allTime == -1:
             self.text = "finished"
+            self.ids["okButton"].disabled = False
             self.ids["stopButton"].disabled = True
             self.ids["pauseButton"].disabled = True
-            self.ids["restartButton"].disabled = True
-            self.ids["startButton"].disabled = False
+            self.sound = SoundLoader.load('alerm.wav')
+            self.sound.play()
+            self.sound.on_stop = self._on_stop
             return False
 
     def startButton(self):
         try:
-            hour = int(self.ids["hour_box"].text)
-            minute = int(self.ids["minute_box"].text)
-            second = int(self.ids["second_box"].text)
+            if self.ids["hour_box"].text == "":
+                hour = 0
+            else:
+                hour = int(self.ids["hour_box"].text)
+            if self.ids["minute_box"].text == "":
+                minute = 0
+            else:
+                minute = int(self.ids["minute_box"].text)
+            if self.ids["second_box"].text == "":
+                second = 0
+            else:
+                second = int(self.ids["second_box"].text)
         except ValueError:
             self.text = "illegal value!"
             return 0
-        allTime = hour * 3600 + minute * 60 + second
-        if allTime < 0:
+        self.allTime = hour * 3600 + minute * 60 + second
+        if self.allTime < 0:
             self.text = "illegal value!"
             return 0
-        self.text = str(allTime)
+        self.hourText = "00" + str(math.floor(int(self.allTime) / 3600))
+        self.minuteText = "00" + str(math.floor(int(self.allTime) % 3600 / 60))
+        self.secondText = "00" + str(math.floor(int(self.allTime) %3600 % 60))
+        self.text = self.hourText[-2:] + ":" + self.minuteText[-2:] + ":" + self.secondText[-2:]
         self.ids["stopButton"].disabled = False
         self.ids["pauseButton"].disabled = False
         self.ids["restartButton"].disabled = True
@@ -66,6 +92,17 @@ class TextWidget(Widget):
         self.ids["pauseButton"].disabled = False
         self.ids["restartButton"].disabled = True
 
+    def okButton(self):
+        self.sound.on_stop = self.passFunction
+        self.sound.stop()
+        self.text = ""
+        self.ids["okButton"].disabled = True
+        self.ids["stopButton"].disabled = True
+        self.ids["pauseButton"].disabled = True
+        self.ids["restartButton"].disabled = True
+        self.ids["startButton"].disabled = False
+
+    
 class MyApp(App):
 
     def __init__(self, **kwargs):
